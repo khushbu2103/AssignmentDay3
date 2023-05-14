d617855c4283d449ff1462fede291e6f320ca089
khushbu <khushbusoni475@gmail.com>
2023-05-15 01:54:40 +0530

    [khushbu] Adding line comperision

---
+++ AssignmentDay3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentDay3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program!");
+
+            float X1, X2, Y1, Y2;
+            float LineLength;
+            Console.WriteLine("please enter x1 number");
+            float.TryParse(Console.ReadLine(), out X1);
+            Console.WriteLine("please enter x2 number");
+            float.TryParse(Console.ReadLine(), out X2);
+            Console.WriteLine("please enter y1 number");
+            float.TryParse(Console.ReadLine(), out Y1);
+            Console.WriteLine("please enter y2 number");
+            float.TryParse(Console.ReadLine(), out Y2);
+
+            LineLength = (float)Math.Sqrt(Math.Pow(X2 - X1, 2) + Math.Pow(Y2 - Y1, 2));
+            Console.WriteLine("length of line{0}:", LineLength);
+            Console.ReadLine();
+
+
+        }
+    }
+}
